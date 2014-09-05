17a4735b70b913f9efca02634df931ec10b7db84
chintancodal <cpatel@codal.com>
2014-09-05 15:51:47 +0530

    this is testing

--- test.php
+++ test.php
@@ -5,6 +5,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-echo 'hello';
+echo "this is git testing";
+echo 'hello 123';
+echo 'hellowowrld';
 exit;
+alisdfhalkjsdhflkjasdkljfasdlkj
